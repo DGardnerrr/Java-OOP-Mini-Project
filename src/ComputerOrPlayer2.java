c33cad61a685819cad9df43711c0fc4697340240
Suresh Melvin Sigera <sureshsigera@gmail.com>
2021-12-09 10:19:13 -0500

    commit

---
+++ src/ComputerOrPlayer2.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+//ComputerOrPlayer2Class inherited
+
+public abstract class ComputerOrPlayer2 extends Player1 {
+    //private variables for ComputerOrPlayer2 class
+    private Random rand;
+    private final int MAX_NUMBER = 3;
+
+    //setter for one of variables to update new info
+    public ComputerOrPlayer2() {
+        setFirstName("They");
+        rand = new Random();
+    }
+    //override method for ComputerOrPlayer2 class - task that ComputerOrPlayer2class must perform
+    //for the game to be executed
+    public void selectStance() {
+        int anyInteger = rand.nextInt(MAX_NUMBER);
+        int randomNumber = 0;
+        switch(randomNumber) {
+            case 0:
+                setStance("ROCK");
+            case 1:
+                setStance("PAPER");
+            case 2:
+                setStance("SCISSORS");
+                break;
+        }
+
+    }}
---
+++ src/GameBoard.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+//created public class for the rock paper scissors game
+//variables were created for the game board
+public class GameBoard {
+    private Player1 pl;
+    private ComputerOrPlayer2 com;
+    private int playerWins;
+    private int playerLosses;
+    private int draws;
+    private boolean isRunning = false;
+    private Scanner scan;
+}
+
+
+
+
+////created public class for the rock paper scissors game
+////variables were created for the game board
+//public class Game {
+//    private Player1 pl;
+//    private ComputerOrPlayer2 com;
+//    private int playerWins;
+//    private int playerLosses;
+//    private int draws;
+//    private boolean isRunning = false;
+//    private Scanner scan;
+//
+//    public Game() {
+//        pl= new Player1();
+//        com = new ComputerOrPlayer2() {
+//            x}
+//    }
+//}
